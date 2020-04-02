93c128ca3fee170e288e7c80a233fd80a0b69bad
adelarmello <adelarmello8@gmail.com>
2020-04-02 20:13:26 -0300

    Criado a rota para  ONG deletar um Incidente.

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -19,5 +19,23 @@ module.exports = {
         });
 
         return response.json({ id });  
+    },
+    async delete(request, response){
+        const { id } = request.params; //pegar o id do incident
+        const ong_id = request.headers.authorization; //pegar o id da ong         
+
+        const incident = await connection('incidents') // procura o incident
+        .where('id', id)
+        .select('ong_id')
+        .first();
+
+       if (incident.ong_id !== ong_id) {
+           return response.status(401).json({ error: 'Operação não permitida!'});
+       }
+       
+       await connection('incidents').where('id', id).delete();
+
+       return response.status(204).send();
     }
+
 };
