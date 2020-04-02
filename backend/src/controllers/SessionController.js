7a4528d0eb3f8c5ca271b7225d57f21b14b58775
adelarmello <adelarmello8@gmail.com>
2020-04-02 20:26:54 -0300

    Criado a rota de Login

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,18 @@
+const connection = require('../database/connection');
+
+module.exports={
+    async create(request, response){
+        const { id } = request.body; //Guarda o ID da ONG que está tentando fazer o login
+
+        const ong = await connection('ongs') //Busca a ONG pelo ID
+            .where('id', id)
+            .select('name')
+            .first();
+        if ( !ong ) {
+            return response.status(400).json({ error: 'Não existe ONG com esse ID!' });
+        }
+
+        return response.json(ong);
+
+    }
+}
