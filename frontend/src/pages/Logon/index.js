634cb1a4d8a52753f76cceb4abbc793bb37d6f00
adelarmello <adelarmello8@gmail.com>
2020-04-03 01:07:30 -0300

    Adicionado a página Logon e sua estilização

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,31 @@
+import React from  'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+
+export default function Logon(){
+    return(
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+                <form>
+                    <h1> Faça seu login</h1>
+
+                    <input className="button" placeholder="Sua ID" />
+                    <button type="submit">Entrar</button>
+
+                    <a href="/register">
+                    <FiLogIn size={16} color = "#E02041" />
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+
+            <img src={heroesImg} alt="Heroes" />
+      </div>
+    
+    );
+}
