f16491383adc0e6d3bd002915285cfb9db79bdee
adelarmello <adelarmello8@gmail.com>
2020-04-13 12:46:30 -0300

    Adicionado alguns testes

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto'); //lib pra gerar uma string aleatória(Nesse caso é pra gerar o ID(números e letras))
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
 module.exports = {
@@ -11,7 +11,7 @@ module.exports = {
     async create (request, response) {
         const {name, email, whatsapp, city, uf} = request.body;
     
-        const id = crypto.randomBytes(4).toString('HEX'); //gera 4 bytes de caracteres decimais(número e letras).
+        const id = generateUniqueId(); 
     
         await connection('ongs').insert({
             id,
