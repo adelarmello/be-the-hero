0e2a74e259f557a85a9718f002392422c973af97
adelarmello <adelarmello8@gmail.com>
2020-04-13 15:12:29 -0300

    Finalizado os teste no Backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,11 @@ const cors    = require('cors');
 const {errors} = require('celebrate');
 const routes  = require('./routes');
 
-const app     = express();
+const app   = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errors());
 
-app.listen(3333);
+module.exports = app;
--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connection = knex(configuration.development); // é a connection de desenvolvimento
+const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
+
+const connection = knex(config); // é a connection de desenvolvimento
 
 module.exports = connection;
